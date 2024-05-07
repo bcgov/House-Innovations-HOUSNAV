562ab46ee88462576e13d661769116254a2d1873
Nicholas Lyon <nicholas.lyon@dynamit.com>
2024-05-07 12:54:19 -0400

    adding basic typography and container styles

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,15 @@ import Link from "@repo/ui/link";
 
 export default function Page(): JSX.Element {
   return (
-    <>
+    <div className="container">
       <br />
       <Link href="/walkthroughes">Walkthroughes</Link>
-    </>
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+      <h5>h5</h5>
+      <p>a paragraph of text would go here</p>
+    </div>
   );
 }
