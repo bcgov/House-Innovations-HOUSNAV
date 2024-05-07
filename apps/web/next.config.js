9fcf0eac8f4d1fdca2902af0f13b222b38c6113d
Nicholas Lyon <nicholas.lyon@dynamit.com>
2024-05-07 10:38:58 -0400

    optimizing react aria internationalization

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
+const localesPlugin = require("@react-aria/optimize-locales-plugin");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Don't include any locale strings in the client JS bundle.
+      config.plugins.push(localesPlugin.webpack({ locales: [] }));
+    }
+    return config;
+  },
 };
