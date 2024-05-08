bcfdfdea292e85ceabd3f13b4c6b5dd7e0da73e9
Lauren Revere <lauren.revere@willowtreeapps.com>
2024-05-08 16:31:20 -0700

    Add shared vitest config

---
+++ vitest.shared.ts
@@ -0,0 +1,12 @@
+
+import { defineProject} from 'vitest/config';
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineProject({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom"
+  },
+});
