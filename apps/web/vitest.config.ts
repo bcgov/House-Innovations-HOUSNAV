3ff7d37a85380512a065c55cd1aeaa26ee9dbd8b
Lauren Revere <lauren.revere@willowtreeapps.com>
2024-05-09 12:12:36 -0700

    Move test configs back to workspace

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineProject, mergeConfig } from "vitest/config";
-import configShared from "../../vitest.shared.ts";
+import { defineProject } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-export default mergeConfig(
-  configShared,
-  defineProject({
-    test: {
-      setupFiles: "./tests/setup.ts",
-    },
-  }),
-);
+export default defineProject({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
+});
