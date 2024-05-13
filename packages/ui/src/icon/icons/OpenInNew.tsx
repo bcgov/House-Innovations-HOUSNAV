9672b06d919c15b7181973d5b2ee82b07df804af
Nicholas Lyon <nicholas.lyon@dynamit.com>
2024-05-13 13:51:00 -0400

    hide icons to keep VO from announcing buttons as groups

--- packages/ui/src/icon/icons/OpenInNew.tsx
+++ packages/ui/src/icon/icons/OpenInNew.tsx
@@ -9,6 +9,7 @@ export default function OpenInNew({ title, id, ...props }: IconProps) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       aria-labelledby={id ? id : undefined}
+      aria-hidden="true"
       {...props}
     >
       {id === undefined || title === undefined ? null : (
