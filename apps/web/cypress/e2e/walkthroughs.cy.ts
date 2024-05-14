de008aced0440ddb0c537c145a8d3aae2a03c912
Lauren Revere <lauren.revere@willowtreeapps.com>
2024-05-14 10:41:57 -0700

    TE-2: Add accessibility testing

--- apps/web/cypress/e2e/walkthroughs.cy.ts
+++ apps/web/cypress/e2e/walkthroughs.cy.ts
@@ -2,4 +2,11 @@ describe("walkthroughs", () => {
   it("loads the homepage", () => {
     cy.visit("/");
   });
+
+  //Skip accessibility test until we have real first page
+  it.skip("should be accessible", () => {
+    cy.visit("/");
+    cy.injectAxe();
+    cy.checkA11y();
+  });
 });
