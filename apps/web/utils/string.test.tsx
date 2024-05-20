9dad755b3e29ab225818a1749db91e60c3fadd01
Nicholas Lyon <nicholas.lyon@dynamit.com>
2024-05-20 15:31:30 -0400

    FE-4: Adding more tests

---
+++ apps/web/utils/string.test.tsx
@@ -0,0 +1,112 @@
+// 3rd part
+import { describe, it, expect } from "vitest";
+import { JSX } from "react";
+// repo
+import { getMultiChoiceQuestion } from "@repo/data/useWalkthroughTestData";
+// local
+import { getStringFromComponents, parseStringToComponents } from "./string";
+
+describe("string", () => {
+  /*
+   * parseStringToComponents
+   */
+  it("parseStringToComponents: defined-term", () => {
+    // get test data
+    const questionData = getMultiChoiceQuestion();
+
+    // check if we have questionText and throw error if we don't
+    if (!questionData?.questionData.questionText) {
+      assert.fail(
+        "question data is missing question text - need to fix test data",
+      );
+    }
+
+    // parse question string
+    let components = parseStringToComponents(
+      questionData?.questionData.questionText,
+    );
+
+    if (typeof components === "string") {
+      assert.fail(
+        "question text is missing defined term - need to fix test data",
+      );
+    }
+
+    // check if components is an array or not
+    if (!Array.isArray(components)) {
+      components = [components];
+    }
+
+    const hasDefinedTerm = components.some((component) => {
+      // ignore if component is a string
+      if (typeof component === "string") {
+        return false;
+      }
+
+      // check if component is DefinedTerm
+      return component.type.name === "DefinedTerm";
+    });
+
+    // expect hasDefinedTerm to be true
+    expect(hasDefinedTerm).toBeTruthy();
+  });
+  it("parseStringToComponents: no defined-term", () => {
+    // parse question string
+    const components = parseStringToComponents(
+      "Question text without a defined term in it.",
+    );
+
+    let componentsArray: JSX.Element[] | string[] | (string | JSX.Element)[] =
+      [];
+    // check if components is an array or not
+    if (!Array.isArray(components)) {
+      componentsArray = [components];
+    }
+
+    const hasDefinedTerm = componentsArray.some((component) => {
+      // ignore if component is a string
+      if (typeof component === "string") {
+        return false;
+      }
+
+      // check if component is DefinedTerm
+      return component.type.name === "DefinedTerm";
+    });
+
+    // expect hasDefinedTerm to be false
+    expect(hasDefinedTerm).toBeFalsy();
+  });
+  /*
+   * getStringFromComponents
+   */
+  it("getStringFromComponents: string", () => {
+    const string = "test string";
+    const result = getStringFromComponents(string);
+    expect(result).toBe(string);
+  });
+  // check with parseStringToComponents
+  it("getStringFromComponents: string with component", () => {
+    const string = "test string";
+    const stringWithComponent = "test <potato>string</potato>";
+    const components = parseStringToComponents(stringWithComponent);
+    const result = getStringFromComponents(components);
+    expect(result).toBe(string);
+  });
+  // null case
+  it("getStringFromComponents: null", () => {
+    const result = getStringFromComponents(null);
+    expect(result).toBe("");
+  });
+  // number case
+  it("getStringFromComponents: number", () => {
+    const number = 123;
+    const result = getStringFromComponents(number);
+    expect(result).toBe(number.toString());
+  });
+  // boolean case
+  it("getStringFromComponents: boolean", () => {
+    const boolean = true;
+    const result = getStringFromComponents(boolean);
+    expect(result).toBe("");
+  });
+});
--- packages/ui/src/radio-group/RadioGroup.test.tsx
+++ packages/ui/src/radio-group/RadioGroup.test.tsx
@@ -1,23 +1,23 @@
 // 3rd party
 import { describe, expect, it } from "vitest";
 import { act, render, waitFor } from "@testing-library/react";
+import { Form } from "react-aria-components";
 // repo
 import {
   GET_TESTID_BUTTON,
+  GET_TESTID_RADIO,
   GET_TESTID_RADIO_GROUP,
   TESTID_BUTTON_SUBMIT,
-  TESTID_RADIO,
   TESTID_RADIO_GROUP_ERROR,
   TESTID_RADIO_GROUP_LABEL,
 } from "@repo/constants/src/testids";
 // workspace
 import RadioGroup from "./RadioGroup";
 import Button from "../button/Button";
+import { userSetupAndRender } from "web/tests/utils";
 
 // test constants
 const RADIO_GROUP_NAME = "storeys";
-const getRadioTestId = (testId = RADIO_GROUP_NAME, value: string) =>
-  `${TESTID_RADIO}-${testId}-${value}`;
 const getRadioGroupLabelTestId = (testId = RADIO_GROUP_NAME) =>
   `${TESTID_RADIO_GROUP_LABEL}-${testId}`;
 const getRadioGroupErrorTestId = (testId = RADIO_GROUP_NAME) =>
@@ -42,8 +42,8 @@ const RADIO_GROUP_OPTIONS = [
 
 describe("RadioGroup", () => {
   // renders
-  it("renders appropriate items with required data and nothing selected", () => {
-    const { getByTestId } = render(
+  it("renders appropriate items with required data and nothing selected", async () => {
+    const { user, getByTestId } = userSetupAndRender(
       <RadioGroup
         options={RADIO_GROUP_OPTIONS}
         label={RADIO_GROUP_LABEL}
@@ -63,12 +63,25 @@ describe("RadioGroup", () => {
 
     // renders radio options with correct values
     RADIO_GROUP_OPTIONS.forEach(({ value }) => {
-      const option = getByTestId(getRadioTestId(RADIO_GROUP_NAME, value));
+      const option = getByTestId(GET_TESTID_RADIO(RADIO_GROUP_NAME, value));
       const input = option.querySelector("input[type='radio']");
       expect(option).toBeInTheDocument();
       expect(input).toHaveAttribute("value", value);
+      expect(input).toHaveAttribute("name", RADIO_GROUP_NAME);
       expect(input).not.toBeChecked();
     });
+
+    // click on radio
+    const option1 = getByTestId(
+      GET_TESTID_RADIO(RADIO_GROUP_NAME, OPTION_1_VALUE),
+    );
+    await act(async () => {
+      await user.click(option1);
+    });
+
+    // check if correct radio is selected
+    const input1 = option1.querySelector("input[type='radio']");
+    expect(input1).toBeChecked();
   });
   // default value
   it("renders item with default value as selected", () => {
@@ -84,11 +97,13 @@ describe("RadioGroup", () => {
 
     // get options and inputs and check if correct one is selected
     const option1 = getByTestId(
-      getRadioTestId(RADIO_GROUP_NAME, OPTION_1_VALUE),
+      GET_TESTID_RADIO(RADIO_GROUP_NAME, OPTION_1_VALUE),
     );
-    const option2 = getByTestId(getRadioTestId(RADIO_GROUP_NAME, defaultValue));
+    const option2 = getByTestId(
+      GET_TESTID_RADIO(RADIO_GROUP_NAME, defaultValue),
+    );
     const option3 = getByTestId(
-      getRadioTestId(RADIO_GROUP_NAME, OPTION_3_VALUE),
+      GET_TESTID_RADIO(RADIO_GROUP_NAME, OPTION_3_VALUE),
     );
     const input1 = option1.querySelector("input[type='radio']");
     const input2 = option2.querySelector("input[type='radio']");
@@ -113,11 +128,13 @@ describe("RadioGroup", () => {
 
     // get options and inputs and check if correct one is selected
     const option1 = getByTestId(
-      getRadioTestId(RADIO_GROUP_NAME, OPTION_1_VALUE),
+      GET_TESTID_RADIO(RADIO_GROUP_NAME, OPTION_1_VALUE),
     );
-    const option2 = getByTestId(getRadioTestId(RADIO_GROUP_NAME, defaultValue));
+    const option2 = getByTestId(
+      GET_TESTID_RADIO(RADIO_GROUP_NAME, defaultValue),
+    );
     const option3 = getByTestId(
-      getRadioTestId(RADIO_GROUP_NAME, selectedValue),
+      GET_TESTID_RADIO(RADIO_GROUP_NAME, selectedValue),
     );
     const input1 = option1.querySelector("input[type='radio']");
     const input2 = option2.querySelector("input[type='radio']");
@@ -127,10 +144,10 @@ describe("RadioGroup", () => {
     expect(input3).toBeChecked();
   });
   // shows error
-  it("renders error message when provided, is required, and not selected on submit", () => {
+  it("renders error message when provided, is required, and not selected on submit", async () => {
     const errorMessage = "Please select a number of storeys";
-    const { getByTestId, queryByTestId } = render(
-      <form>
+    const { user, getByTestId, queryByTestId, getByText } = userSetupAndRender(
+      <Form>
         <RadioGroup
           options={RADIO_GROUP_OPTIONS}
           label={RADIO_GROUP_LABEL}
@@ -141,22 +158,20 @@ describe("RadioGroup", () => {
         <Button type={"submit"} data-testid={TESTID_BUTTON_SUBMIT}>
           Submit
         </Button>
-      </form>,
+      </Form>,
     );
 
     // no error message
     expect(queryByTestId(getRadioGroupErrorTestId())).not.toBeInTheDocument();
 
     // submit form
-    act(() => {
-      getByTestId(GET_TESTID_BUTTON(TESTID_BUTTON_SUBMIT)).click();
+    await act(async () => {
+      await user.click(getByTestId(GET_TESTID_BUTTON(TESTID_BUTTON_SUBMIT)));
     });
 
     // error message
-    waitFor(() => {
-      expect(getByTestId(getRadioGroupErrorTestId())).toHaveTextContent(
-        errorMessage,
-      );
+    await waitFor(() => {
+      expect(getByText(errorMessage)).toBeInTheDocument();
     });
   });
   // hides label
@@ -171,6 +186,29 @@ describe("RadioGroup", () => {
     );
 
     expect(queryByTestId(getRadioGroupLabelTestId())).toHaveClass("u-hidden");
+  });
+  // shows name when no label
+  it("shows name when no label value provided", () => {
+    const { getByTestId } = render(
+      <RadioGroup options={RADIO_GROUP_OPTIONS} name={RADIO_GROUP_NAME} />,
+    );
+
+    expect(getByTestId(getRadioGroupLabelTestId())).toHaveTextContent(
+      RADIO_GROUP_NAME,
+    );
+  });
+  // no label
+  it("does not render label when noLabel is true", () => {
+    const { queryByTestId } = render(
+      <RadioGroup
+        options={RADIO_GROUP_OPTIONS}
+        label={RADIO_GROUP_LABEL}
+        name={RADIO_GROUP_NAME}
+        noLabel
+      />,
+    );
+
+    expect(queryByTestId(getRadioGroupLabelTestId())).not.toBeInTheDocument();
   });
   // custom testid
   it("renders custom testid", () => {
