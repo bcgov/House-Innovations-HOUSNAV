09590a2a1494b8177f5dd353b370027c9b14a517
Lauren Revere <lauren.revere@willowtreeapps.com>
2024-06-05 14:53:37 -0700

    HOUSNAV-25: Add basic cypress test for walkthrough 1

--- apps/web/cypress/e2e/walkthroughs.cy.ts
+++ apps/web/cypress/e2e/walkthroughs.cy.ts
@@ -1,11 +1,30 @@
-describe("walkthroughs", () => {
-  it("loads the homepage", () => {
-    cy.visit("/");
+describe("walkthrough 1", () => {
+  beforeEach(() => {
+    cy.visit("/walkthrough/9.9.9");
   });
 
-  //Skip accessibility test until we have real first page
-  it.skip("should be accessible", () => {
-    cy.visit("/");
+  it("can be navigated", () => {
+    // select and submit an answer for the first question
+    cy.get('[data-testid="radio-P01-notSure"]').click();
+    cy.get('[data-testid="button-question-submit"]').click();
+
+    // confirm we are showing the second question
+    cy.get('[data-testid="checkbox-group-P02"]');
+  });
+
+  it("default state should be accessible", () => {
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+
+  it("error state should be accessible", () => {
+    // press submit button without selecting a radio button to show error state
+    cy.get('[data-testid="button-question-submit"]').click();
+
+    // confirm error message is visible
+    // TODO - replace with TESTID_RADIO_GROUP_ERROR once it works
+    cy.get(".ui-RadioGroup--Error");
+
     cy.injectAxe();
     cy.checkA11y();
   });
