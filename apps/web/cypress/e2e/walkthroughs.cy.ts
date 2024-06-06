3faf0388325ae33a4b88a26689ad1e384df8cfca
Lauren Revere <lauren.revere@willowtreeapps.com>
2024-06-06 10:42:05 -0700

    HOUSENAV-25: Use testids

--- apps/web/cypress/e2e/walkthroughs.cy.ts
+++ apps/web/cypress/e2e/walkthroughs.cy.ts
@@ -1,3 +1,9 @@
+import {
+  GET_TESTID_RADIO,
+  TESTID_QUESTION_SUBMIT,
+  GET_TESTID_CHECKBOX_GROUP,
+} from "@repo/constants/src/testids";
+
 describe("walkthrough 1", () => {
   beforeEach(() => {
     cy.visit("/walkthrough/9.9.9");
@@ -5,11 +11,12 @@ describe("walkthrough 1", () => {
 
   it("can be navigated", () => {
     // select and submit an answer for the first question
-    cy.getByTestID("radio-P01-notSure").click();
-    cy.getByTestID("button-question-submit").click();
+    // TODO - randomize which answer is selected
+    cy.getByTestID(GET_TESTID_RADIO("P01", "yes")).click();
+    cy.getByGeneralTestID(TESTID_QUESTION_SUBMIT).click();
 
     // confirm we are showing the second question
-    cy.getByTestID("checkbox-group-P02");
+    cy.getByTestID(GET_TESTID_CHECKBOX_GROUP("P02"));
   });
 
   it("default state should be accessible", () => {
@@ -19,7 +26,7 @@ describe("walkthrough 1", () => {
 
   it("error state should be accessible", () => {
     // press submit button without selecting a radio button to show error state
-    cy.getByTestID("button-question-submit").click();
+    cy.getByGeneralTestID(TESTID_QUESTION_SUBMIT).click();
 
     // confirm error message is visible
     // TODO - replace with TESTID_RADIO_GROUP_ERROR once it works
