1fda9f469ac8ff6303e21a86adeadeef7ff0c385
joshua booker <126275279+jbook24@users.noreply.github.com>
2024-06-11 15:10:16 -0500

    draft
    initial commit

---
+++ packages/data/json/9.10.14.test.ts
@@ -0,0 +1,46 @@
+// 3rd party
+import { describe, it, expect } from "vitest";
+// local
+import { isWalkthroughItemTypeVariable } from "../src/useWalkthroughData";
+import data from "../walkthroughs/9.10.14.json";
+
+describe("Data - 9.10.14", () => {
+  it("verify all questions either appear in a section or are of variable type", () => {
+    // iterate through each question and check if it appears in a section or is of variable type
+    const possibleUnusedQuestion = Object.entries(data.questions).find(
+      ([questionId, question]) => {
+        if (isWalkthroughItemTypeVariable(question.walkthroughItemType)) {
+          return false;
+        }
+
+        return !Object.values(data.sections).some((section) =>
+          section.sectionQuestions.includes(questionId),
+        );
+      },
+    );
+    if (possibleUnusedQuestion)
+      console.log("Possible unused question ID:", possibleUnusedQuestion[0]);
+
+    // expect possibleUnusedQuestion to be undefined
+    expect(possibleUnusedQuestion).toBe(undefined);
+  });
+  it("verify all sectionQuestions are valid question ids", () => {
+    // iterate through each section and check if all sectionQuestions are valid question ids
+    const sectionWithUnknownQuestion = Object.values(data.sections).find(
+      (section) => {
+        const unknownQuestion = section.sectionQuestions.find(
+          (questionId) => !Object.keys(data.questions).includes(questionId),
+        );
+        if (unknownQuestion) {
+          console.log("Section:", section.sectionTitle);
+          console.log("Unknown question", unknownQuestion);
+        }
+
+        return !!unknownQuestion;
+      },
+    );
+
+    // expect sectionWithUnknownQuestion to be undefined
+    expect(sectionWithUnknownQuestion).toBe(undefined);
+  });
+});
