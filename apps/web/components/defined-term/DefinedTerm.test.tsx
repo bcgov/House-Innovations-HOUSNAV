afa3bb06369227fca0352872734f0e88f5ae94f9
Matthew Ward <wardm89@yahoo.com>
2024-06-12 12:27:47 -0400

    Adds Modal Side component with auto scroll
    spacing adjustment
    
    add dummy scrollIntoView for tests

--- apps/web/components/defined-term/DefinedTerm.test.tsx
+++ apps/web/components/defined-term/DefinedTerm.test.tsx
@@ -5,19 +5,44 @@ import { render } from "@testing-library/react";
 import {
   GET_TESTID_BUTTON,
   TESTID_DEFINED_TERM,
+  TESTID_MODAL_SIDE,
 } from "@repo/constants/src/testids";
 // local
 import DefinedTerm from "./DefinedTerm";
+import userEvent from "@testing-library/user-event";
 
 describe("DefinedTerm", () => {
   it("renders with data-term attribute", () => {
     const testTerm = "test-term";
     const { getByTestId } = render(
-      <DefinedTerm term={testTerm}>Test Term</DefinedTerm>,
+      <DefinedTerm term={testTerm}>Test Term</DefinedTerm>
     );
     const button = getByTestId(
-      GET_TESTID_BUTTON(`${TESTID_DEFINED_TERM}-${testTerm}`),
+      GET_TESTID_BUTTON(`${TESTID_DEFINED_TERM}-${testTerm}`)
     );
     expect(button).toHaveAttribute("data-term", "test-term");
+  });
+
+  it("opens and closes model when defined-term clicked", async () => {
+    window.HTMLElement.prototype.scrollIntoView = function () {};
+
+    const testTerm = "test-term";
+    const { getByTestId } = render(
+      <DefinedTerm term={testTerm}>Test Term</DefinedTerm>
+    );
+    const button = getByTestId(
+      GET_TESTID_BUTTON(`${TESTID_DEFINED_TERM}-${testTerm}`)
+    );
+
+    await userEvent.click(button);
+    const modal = getByTestId(TESTID_MODAL_SIDE);
+    expect(modal).toBeInTheDocument();
+
+    const closeButton = getByTestId(
+      GET_TESTID_BUTTON(`${TESTID_MODAL_SIDE}-close-button`)
+    );
+
+    await userEvent.click(closeButton);
+    expect(modal).not.toBeInTheDocument();
   });
 });
--- packages/ui/src/modal-side/ModalSide.tsx
+++ packages/ui/src/modal-side/ModalSide.tsx
@@ -1,51 +1,119 @@
 "use client";
-import { Dialog, DialogTrigger, Heading, Modal, ModalOverlay} from 'react-aria-components';
 
+import { useEffect, useRef, ReactNode, useState } from "react";
+import {
+  Dialog,
+  DialogTrigger,
+  Heading,
+  Modal,
+  ModalOverlay,
+} from "react-aria-components";
+
+import { TESTID_MODAL_SIDE } from "@repo/constants/src/testids";
 
 import "./ModalSide.css";
-import Icon from '../icon/Icon';
-import Button from '../button/Button';
+import Icon from "../icon/Icon";
+import Button from "../button/Button";
+import { parseStringToComponents } from "web/utils/string";
 
 export interface ModalSideProps {
-  sections: { number: string, header: string, content: string }[];
+  triggerContent: ReactNode;
+  sections: { number: string; header: string; content: string }[];
+  scrollToSection?: string;
+  "data-testid"?: string;
 }
 
-export default function ModalSide({sections, ...props }: ModalSideProps) {
+export default function ModalSide({
+  triggerContent,
+  sections,
+  scrollToSection,
+  "data-testid": testid = TESTID_MODAL_SIDE,
+  ...props
+}: ModalSideProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [focusSection, setFocusSection] = useState(scrollToSection);
+  const [highlightedSection, setHighlightedSection] = useState<string | null>(
+    null
+  );
+
+  const modalRef = useRef<HTMLDivElement>(null);
+  const sectionRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+
+  useEffect(() => {
+    if (focusSection && sectionRefs.current[focusSection]) {
+      const sectionElement = sectionRefs.current[focusSection];
+      sectionElement?.scrollIntoView({
+        behavior: "instant",
+        block: "center",
+      });
+      setHighlightedSection(focusSection);
+    }
+  }, [isOpen, focusSection]);
+
   return (
-    <DialogTrigger >
-    <Button>Open modal</Button>
+    <DialogTrigger onOpenChange={setIsOpen}>
+      {triggerContent}
 
-    <ModalOverlay className="my-overlay" >
-      <Modal className="my-modal" isKeyboardDismissDisabled>
-        <Dialog className="react-aria-Dialog">
-          {({ close }) => <>
-            <Button
-              isIconButton
-              variant="secondary"
-              className="close-button"
-              onPress={close}
-            >
-              <Icon type="close" />
-            </Button>
-            <div className="modal-header"></div>
-            <div className="modal-content">
-              {sections.map((section, index) => (
-                <div key={index} className="modal-section">
-                  <div className="section-header-line">
-                    <Heading level={3} className="section-number">{section.number}</Heading>
-                    <Heading className="section-header">{section.header}</Heading>
-                  </div>
-                  <div className="section-content-line">
-                    <p className="section-content">{section.content}</p>
-                  </div>
+      <ModalOverlay className="ui-ModalSide--Overlay">
+        <Modal
+          className="ui-ModalSide--Modal"
+          ref={modalRef}
+          data-testid={testid}
+        >
+          <Dialog className="ui-ModalSide--AriaDialog">
+            {({ close }) => (
+              <>
+                <div className="ui-ModalSide--Header">
+                  <Button
+                    isIconButton
+                    variant="tertiary"
+                    className="ui-ModalSide--CloseButton"
+                    onPress={close}
+                    data-testid={`${testid}-close-button`}
+                  >
+                    <Icon type="close" />
+                  </Button>
                 </div>
-              ))}
-            </div>
-          </>}
-        </Dialog>
-      </Modal>
-    </ModalOverlay>
-  </DialogTrigger>
-
+                <div className="ui-ModalSide--Content">
+                  {sections.map((section, index) => (
+                    <div
+                      key={index}
+                      ref={(el) => {
+                        sectionRefs.current[section.number] = el;
+                      }}
+                      className={`ui-ModalSide--Section ${
+                        highlightedSection == section.number
+                          ? "ui-ModalSide--SectionHighlighted"
+                          : ""
+                      }`}
+                    >
+                      <div className="ui-ModalSide--SectionHeaderLine">
+                        <Heading
+                          level={3}
+                          className="ui-ModalSide--SectionNumber"
+                        >
+                          {section.number}
+                        </Heading>
+                        <Heading className="ui-ModalSide--SectionHeader">
+                          {section.header}
+                        </Heading>
+                      </div>
+                      <div className="ui-ModalSide--SectionContentLine">
+                        <p className="ui-ModalSide--SectionContent">
+                          {parseStringToComponents(
+                            section.content,
+                            setFocusSection
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </DialogTrigger>
   );
 }
