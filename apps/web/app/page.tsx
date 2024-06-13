2c25db2361a0959ef4a7ce9167417808ada4c728
Matthew Ward <wardm89@yahoo.com>
2024-06-13 10:34:49 -0400

    Remove duplicate card logic

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,28 +3,19 @@ import { JSX } from "react";
 // repo
 import { URL_WALKTHROUGH_HREF } from "@repo/constants/src/urls";
 import WalkthroughCard from "@repo/ui/walkthrough-card";
-import {
-  Walkthroughs,
-  WalkthroughJSONData,
-} from "../../../packages/data/src/useWalkthroughData";
+import { WalkthroughJSONData } from "../../../packages/data/src/useWalkthroughData";
 
 export default function Page(): JSX.Element {
   return (
     <div className="u-container">
       <div>
-        {Object.entries(WalkthroughJSONData).map(([id, data]) => {
-          const info = data.info;
-          const canDisplayCard =
-            info && info.title && info.subtitle && info.description;
+        {Object.entries(WalkthroughJSONData).map(([id, { info }]) => {
           return (
-            canDisplayCard && (
-              <WalkthroughCard
-                key={id}
-                id={id}
-                data={info}
-                href={`${URL_WALKTHROUGH_HREF}/${id}/`}
-              />
-            )
+            <WalkthroughCard
+              id={id}
+              data={info}
+              href={`${URL_WALKTHROUGH_HREF}/${id}/`}
+            />
           );
         })}
       </div>
