5f65001673ece1d016999171a88743de05b077b1
Matthew Ward <wardm89@yahoo.com>
2024-06-13 16:11:29 -0400

    Use next js remote patterns to approve image urls
    Adds alt to tooltip

--- packages/ui/src/image/Image.tsx
+++ packages/ui/src/image/Image.tsx
@@ -1,16 +1,9 @@
-import React from "react";
+import NextImage, { ImageProps } from "next/image";
 
 import { IMAGES_BASE_PATH } from "@repo/constants/src/constants";
 
-export interface ImageProps
-  extends Pick<
-    React.ImgHTMLAttributes<HTMLImageElement>,
-    "id" | "className" | "alt" | "height" | "width"
-  > {
-  src: string;
-}
+export default function Image({ src, ...props }: ImageProps) {
+  const url = src.toString().includes("/") ? src : `${IMAGES_BASE_PATH}/${src}`;
 
-export default function Image({ src, alt = "", ...props }: ImageProps) {
-  const url = src.includes("/") ? src : `${IMAGES_BASE_PATH}/${src}`;
-  return <img src={url} {...props} alt={alt} />;
+  return <NextImage src={url} {...props} />;
 }
