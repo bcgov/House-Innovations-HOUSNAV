6bfc12199439fa9b359bb497ddbcc208a7d5db2c
Matthew Ward <wardm89@yahoo.com>
2024-07-10 14:19:13 -0400

    Add skeleton pdf download component

--- apps/web/components/result/Result.tsx
+++ apps/web/components/result/Result.tsx
@@ -2,14 +2,18 @@
 // repo
 import { TESTID_RESULT } from "@repo/constants/src/testids";
 // local
+import PdfDownload from "@repo/ui/pdf-download";
 import "./Result.css";
 
 export default function Result({ displayMessage }: { displayMessage: string }) {
   return (
-    <div data-testid={TESTID_RESULT} className="web-Result">
-      <div className="u-container-walkthrough">
-        <p>{displayMessage}</p>
+    <>
+      <div data-testid={TESTID_RESULT} className="web-Result">
+        <div className="u-container-walkthrough">
+          <p>{displayMessage}</p>
+        </div>
+        <PdfDownload />
       </div>
-    </div>
+    </>
   );
 }
