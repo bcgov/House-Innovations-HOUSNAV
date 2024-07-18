396713fc3f02d4369b1474b7f07c6a3f9b48003f
Lauren Revere <lauren.revere@willowtreeapps.com>
2024-07-18 00:20:56 -0700

    HOUSNAV-94: Add test that verifies back functionality

--- apps/web/cypress/e2e/9.10.14.cy.ts
+++ apps/web/cypress/e2e/9.10.14.cy.ts
@@ -1,8 +1,4 @@
-import {
-  TESTID_WALKTHROUGH_FOOTER_NEXT,
-  GET_TESTID_CHECKBOX,
-  GET_TESTID_RADIO,
-} from "@repo/constants/src/testids";
+import { TESTID_WALKTHROUGH_FOOTER_BACK } from "@repo/constants/src/testids";
 
 import { runWalkthrough } from "../support/helpers";
 
@@ -21,24 +17,33 @@ describe("walkthrough 2", () => {
     });
   });
 
-  it("error state should be accessible", () => {
-    cy.getByTestID(GET_TESTID_RADIO("P01", "notSure")).click();
-    cy.getByGeneralTestID(TESTID_WALKTHROUGH_FOOTER_NEXT).click();
+  it("new walkthrough works correctly after using back button", () => {
+    /**
+     * This test is designed to test that backing doesn't yeild incorrect data. the back button functionality.
+     * In theory you can use any walkthroughs here, but if they both take the same path for most of it, you will have trouble on checkbox screens.
+     * TODO - Update checkbox screens to unselect unused options
+     * TODO - Make selecting walkthroughs less brittle
+     */
+    const firstWalkthrough = walkthroughs[12];
+    const secondWalkthrough = walkthroughs[16];
 
-    // Select both "none" and another option
-    cy.getInputByTestID(GET_TESTID_CHECKBOX("P02", "17")).click({
-      force: true,
-    });
-    cy.getInputByTestID(GET_TESTID_CHECKBOX("P02", "18")).click({
-      force: true,
-    });
+    // Run the first walkthrough
+    if (firstWalkthrough) {
+      runWalkthrough(firstWalkthrough, results.workflow2);
 
-    // Show error message
-    cy.getByGeneralTestID(TESTID_WALKTHROUGH_FOOTER_NEXT).click();
-
-    // Test accessibility
-    cy.injectAxe();
-    cy.checkA11y();
+      // Go back to the beginning
+      for (let i = 0; i < firstWalkthrough.steps.length; i++) {
+        cy.getByGeneralTestID(TESTID_WALKTHROUGH_FOOTER_BACK).click();
+      }
+    } else {
+      throw new Error("Walkthrough does not exist in workflow data");
+    }
+    // Run the second walkthrough
+    if (secondWalkthrough) {
+      runWalkthrough(secondWalkthrough, results.workflow2);
+    } else {
+      throw new Error("Walkthrough does not exist in workflow data");
+    }
   });
 
   it("default state should be accessible", () => {
