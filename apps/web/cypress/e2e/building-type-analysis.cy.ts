f1c4de826f2f614d53887aa8c15b4fdf082c2e53
stechkev <kevin.stechler@willowtreeapps.com>
2024-10-25 16:48:17 -0400

    HOUSNAV-211: Adding in tests for the final page of the building type analysis screen. small refactors to helper methods to make the navigation and assertions less tightly coupled

---
+++ apps/web/cypress/e2e/building-type-analysis.cy.ts
@@ -0,0 +1,45 @@
+import {
+  getWalkthrough,
+  navigateThroughWalkthrough,
+  runWalkthrough,
+} from "../support/helpers";
+import { walkthroughs } from "../fixtures/building-type-analysis-test-data.json";
+import { results } from "../fixtures/results-data.json";
+import {
+  GET_TESTID_BUTTON,
+  GET_TESTID_LINK,
+  GET_TESTID_RESULT_BANNER,
+  TESTID_RESULT_CONTINUE,
+  TESTID_RESULT_RETURN_TO_HOME,
+  TESTID_WALKTHROUGH_FOOTER_START_OVER,
+} from "@repo/constants/src/testids";
+import { STR_BUILDING_TYPE_ANALYSIS_ID } from "@repo/constants/src/constants";
+
+describe("building type analysis tests", () => {
+  beforeEach(() => {
+    cy.visit("/building-type-analysis");
+  });
+
+  // Test all walkthroughs defined in test data
+  walkthroughs.forEach((walkthrough) => {
+    it(walkthrough.title, () => {
+      runWalkthrough(walkthrough, results.building_type_analysis);
+    });
+  });
+
+  it("should display correct ui elements on last page", () => {
+    navigateThroughWalkthrough(getWalkthrough(walkthroughs, 0));
+    cy.getByTestID(
+      GET_TESTID_RESULT_BANNER(STR_BUILDING_TYPE_ANALYSIS_ID),
+    ).should("be.visible");
+    cy.getByTestID(GET_TESTID_LINK(TESTID_RESULT_RETURN_TO_HOME)).should(
+      "be.visible",
+    );
+    cy.getByTestID(GET_TESTID_LINK(TESTID_RESULT_CONTINUE)).should(
+      "be.visible",
+    );
+    cy.getByTestID(
+      GET_TESTID_BUTTON(TESTID_WALKTHROUGH_FOOTER_START_OVER),
+    ).should("be.visible");
+  });
+});
